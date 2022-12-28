160b4196662f370ea6209538b52e11e42225faa8
Igor Kochenko <igorivanov76@yahoo.com>
2022-12-28 21:04:27 +0200

    java

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 package com.epam.rd.autotasks.meetautocode;
 public class HelloJava {
     public static void main(String[] args) {
        System.out.print("Hello, Java course!");
     }
 }
+=======
+package com.epam.rd.autotasks.meetautocode;
+public class HelloJava {
+    public static void main(String[] args) {
+       System.out.print("Hello, Java course!");
+    }
+}
+>>>>>>> 87df433726f00f172966ea2e9742499aa548a780
